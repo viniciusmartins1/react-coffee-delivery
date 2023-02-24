fa74ce033b5906806b044395769f789200ce1c9d
Vinicius Martins <vm3090474@gmail.com>
2023-02-23 21:58:49 -0300

    feat: routes configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
 
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+import { Router } from "./Router";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>Hello World</div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
   );
 }
