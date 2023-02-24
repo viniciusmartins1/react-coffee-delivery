ce54b0aa40fa5e6cc5fa6f1fbcc8b2384c1a9a02
Vinicius Martins <vm3090474@gmail.com>
2023-02-23 21:14:12 -0300

    style: configuração dos estilos e cores globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "styled-components";
+
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <div>Hello World</div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
