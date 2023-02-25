49172e44139cd79bf2797dc04befae81423009de
Vinicius Martins <vm3090474@gmail.com>
2023-02-24 22:51:30 -0300

    feat: main content layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
+import { MapPin, ShoppingCart, Timer, Package, Coffee } from "phosphor-react";
 import logoImage from "../../assets/logo.png";
-import { MapPin, ShoppingCart } from "phosphor-react";
+import imgCoffee from "../../assets/img-coffee-home.png";
+import backgroundHome from "../../assets/background-home.png";
 
 import {
   HeaderContainer,
@@ -7,24 +9,61 @@ import {
   LocationContainer,
   ShoppingCartContainer,
   HomeContainer,
+  MainContent,
+  ItemsContainer,
+  ItemStyle,
+  ItemLeftStyle,
+  HomeBody,
 } from "./styles";
 
 export function Home() {
   return (
-    <HomeContainer>
-      <HeaderContainer>
-        <img src={logoImage} alt="" />
-        <ActionsConteiner>
-          <LocationContainer>
-            <MapPin size={32} weight="fill" />
-            <p>Porto Alegre, RS</p>
-          </LocationContainer>
-          <ShoppingCartContainer>
-            <ShoppingCart size={32} weight="fill" />
-            <span>3</span>
-          </ShoppingCartContainer>
-        </ActionsConteiner>
-      </HeaderContainer>
-    </HomeContainer>
+    <HomeBody>
+      <HomeContainer>
+        <HeaderContainer>
+          <img src={logoImage} alt="" />
+          <ActionsConteiner>
+            <LocationContainer>
+              <MapPin size={32} weight="fill" />
+              <p>Porto Alegre, RS</p>
+            </LocationContainer>
+            <ShoppingCartContainer>
+              <ShoppingCart size={32} weight="fill" />
+              <span>3</span>
+            </ShoppingCartContainer>
+          </ActionsConteiner>
+        </HeaderContainer>
+        <MainContent>
+          <article>
+            <div>
+              <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+              <p>
+                Com o Coffee Delivery você recebe seu café onde estiver, a
+                qualquer hora
+              </p>
+            </div>
+            <ItemsContainer>
+              <ItemStyle backgroundColor="yellowDark">
+                <ShoppingCart size={18} weight="fill" />
+                <p>Compra simples e segura</p>
+              </ItemStyle>
+              <ItemLeftStyle backgroundColor="gray">
+                <Package size={18} weight="fill" />
+                <p>Embalagem mantém o café intacto</p>
+              </ItemLeftStyle>
+              <ItemStyle backgroundColor="yellow">
+                <Timer size={18} weight="fill" />
+                <p>Entrega rápida e rastreada</p>
+              </ItemStyle>
+              <ItemLeftStyle backgroundColor="purple">
+                <Coffee size={18} weight="fill" />
+                <p>O café chega fresquinho até você</p>
+              </ItemLeftStyle>
+            </ItemsContainer>
+          </article>
+          <img src={imgCoffee} alt="" />
+        </MainContent>
+      </HomeContainer>
+    </HomeBody>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,5 +1,11 @@
 import styled from "styled-components";
+import backgroundHome from "../../assets/background-home.png";
 
+export const HomeBody = styled.div`
+  background-image: url(${backgroundHome});
+  background-position: bottom;
+  background-size: contain;
+`;
 export const HomeContainer = styled.div`
   max-width: 1220px;
   height: 100vh;
@@ -56,4 +62,76 @@ export const ShoppingCartContainer = styled.div`
     top: -14px;
     right: -12px;
   }
+`;
+
+export const MainContent = styled.main`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: 92px 0;
+
+  article {
+    width: 588px;
+  }
+
+  h1 {
+    color: ${({ theme }) => theme["gray-900"]};
+    font-family: "Baloo 2";
+    font-weight: 800;
+    font-size: 48px;
+    font-style: normal;
+    line-height: 130%;
+  }
+
+  h1 + p {
+    margin: 16px 0 66px 0;
+    color: ${({ theme }) => theme["gray-800"]};
+    font-size: 20px;
+  }
+`;
+
+export const ItemsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 25px;
+  align-items: center;
+  flex-direction: row;
+`;
+
+const BACKGROUND_COLOR = {
+  yellowDark: "yellow-700",
+  yellow: "yellow-500",
+  purple: "purple-500",
+  gray: "gray-700",
+} as const;
+
+interface ItemProps {
+  backgroundColor: keyof typeof BACKGROUND_COLOR;
+}
+
+export const ItemStyle = styled.div<ItemProps>`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  flex-direction: row;
+  width: 256px;
+
+  p {
+    font-size: 1rem;
+  }
+
+  svg {
+    color: ${({ theme }) => theme["gray-100"]};
+    background-color: ${(props) =>
+      props.theme[BACKGROUND_COLOR[props.backgroundColor]]};
+    padding: 8px 8px;
+    box-sizing: content-box;
+    border-radius: 50%;
+  }
+`;
+
+export const ItemLeftStyle = styled(ItemStyle)`
+  width: 300px;
 `;
