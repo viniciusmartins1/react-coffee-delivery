b6b966783691aaecc68ee454a879a3a59f180403
Vinicius Martins <vm3090474@gmail.com>
2023-02-28 23:27:52 -0300

    feat: create car coffee layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,16 @@
-import { MapPin, ShoppingCart, Timer, Package, Coffee } from "phosphor-react";
+import {
+  MapPin,
+  ShoppingCart,
+  Timer,
+  Package,
+  Coffee,
+  ShoppingCartSimple,
+  Plus,
+  Minus,
+} from "phosphor-react";
 import logoImage from "../../assets/logo.png";
 import imgCoffee from "../../assets/img-coffee-home.png";
-import backgroundHome from "../../assets/background-home.png";
+import coffeeItemImg from "../../assets/coffee.png";
 
 import {
   HeaderContainer,
@@ -14,6 +23,14 @@ import {
   ItemStyle,
   ItemLeftStyle,
   HomeBody,
+  ContentCoffee,
+  CoffeeListContent,
+  CoffeeItemContent,
+  CoffeeItemTags,
+  CoffeeItemTitle,
+  CoffeeItemDescription,
+  CoffeeItemBuy,
+  TitleList,
 } from "./styles";
 
 export function Home() {
@@ -34,34 +51,66 @@ export function Home() {
           </ActionsConteiner>
         </HeaderContainer>
         <MainContent>
-          <article>
-            <div>
-              <h1>Encontre o café perfeito para qualquer hora do dia</h1>
-              <p>
-                Com o Coffee Delivery você recebe seu café onde estiver, a
-                qualquer hora
-              </p>
-            </div>
-            <ItemsContainer>
-              <ItemStyle backgroundColor="yellowDark">
-                <ShoppingCart size={18} weight="fill" />
-                <p>Compra simples e segura</p>
-              </ItemStyle>
-              <ItemLeftStyle backgroundColor="gray">
-                <Package size={18} weight="fill" />
-                <p>Embalagem mantém o café intacto</p>
-              </ItemLeftStyle>
-              <ItemStyle backgroundColor="yellow">
-                <Timer size={18} weight="fill" />
-                <p>Entrega rápida e rastreada</p>
-              </ItemStyle>
-              <ItemLeftStyle backgroundColor="purple">
-                <Coffee size={18} weight="fill" />
-                <p>O café chega fresquinho até você</p>
-              </ItemLeftStyle>
-            </ItemsContainer>
-          </article>
-          <img src={imgCoffee} alt="" />
+          <ContentCoffee>
+            <article>
+              <div>
+                <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+                <p>
+                  Com o Coffee Delivery você recebe seu café onde estiver, a
+                  qualquer hora
+                </p>
+              </div>
+              <ItemsContainer>
+                <ItemStyle backgroundColor="yellowDark">
+                  <ShoppingCart size={18} weight="fill" />
+                  <p>Compra simples e segura</p>
+                </ItemStyle>
+                <ItemLeftStyle backgroundColor="gray">
+                  <Package size={18} weight="fill" />
+                  <p>Embalagem mantém o café intacto</p>
+                </ItemLeftStyle>
+                <ItemStyle backgroundColor="yellow">
+                  <Timer size={18} weight="fill" />
+                  <p>Entrega rápida e rastreada</p>
+                </ItemStyle>
+                <ItemLeftStyle backgroundColor="purple">
+                  <Coffee size={18} weight="fill" />
+                  <p>O café chega fresquinho até você</p>
+                </ItemLeftStyle>
+              </ItemsContainer>
+            </article>
+            <img src={imgCoffee} alt="" />
+          </ContentCoffee>
+
+          <TitleList>Nossos cafés</TitleList>
+
+          <CoffeeListContent>
+            <CoffeeItemContent>
+              <img src={coffeeItemImg} alt="" />
+              <CoffeeItemTags>
+                <span>tradicional</span>
+                <span>com leite</span>
+              </CoffeeItemTags>
+              <CoffeeItemTitle>Expresso Tradicional</CoffeeItemTitle>
+              <CoffeeItemDescription>
+                O tradicional café feito com água quente e grãos moídos
+              </CoffeeItemDescription>
+              <CoffeeItemBuy>
+                <p>
+                  <span>R$</span> 9,90
+                </p>
+
+                <div>
+                  <Minus size={14} weight="bold" />
+                  <span>1</span>
+                  <Plus size={14} weight="bold" />
+                </div>
+                <span>
+                  <ShoppingCartSimple size={20} weight="fill" />
+                </span>
+              </CoffeeItemBuy>
+            </CoffeeItemContent>
+          </CoffeeListContent>
         </MainContent>
       </HomeContainer>
     </HomeBody>
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -66,14 +66,12 @@ export const ShoppingCartContainer = styled.div`
 
 export const MainContent = styled.main`
   display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+  flex-direction: column;
 
   padding: 92px 0;
 
   article {
-    width: 588px;
+    width: 688px;
   }
 
   h1 {
@@ -90,6 +88,11 @@ export const MainContent = styled.main`
     color: ${({ theme }) => theme["gray-800"]};
     font-size: 20px;
   }
+`;
+
+export const ContentCoffee = styled.div`
+  display: flex;
+  padding-bottom: 78px;
 `;
 
 export const ItemsContainer = styled.div`
@@ -134,4 +137,125 @@ export const ItemStyle = styled.div<ItemProps>`
 
 export const ItemLeftStyle = styled(ItemStyle)`
   width: 300px;
+`;
+
+export const TitleList = styled.h2`
+  margin-top: 32px;
+  font-family: "Baloo 2";
+  font-size: 32px;
+  line-height: 41.6px;
+  color: ${({ theme }) => theme["gray-800"]};
+`;
+export const CoffeeListContent = styled.div`
+  display: flex;
+  /* justify-content: center; */
+  gap: 32px;
+  flex-wrap: wrap;
+  margin-top: 64px;
+`;
+
+export const CoffeeItemContent = styled.div`
+  background-color: ${({ theme }) => theme["gray-200"]};
+  border-radius: 8px 36px;
+  width: 256px;
+  text-align: center;
+  margin-bottom: 20px;
+
+  padding: 0 20px 20px 20px;
+
+  img {
+    margin-top: -30px;
+    margin-bottom: 10px;
+  }
+`;
+
+export const CoffeeItemTags = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  justify-content: center;
+
+  span {
+    background-color: ${({ theme }) => theme["yellow-100"]};
+    color: ${({ theme }) => theme["yellow-700"]};
+    /* margin: 10px 0 16px 0; */
+
+    border-radius: 20px;
+
+    padding: 4px 8px;
+    text-transform: uppercase;
+    font-size: 10px;
+    font-weight: bold;
+    line-height: 13px;
+  }
+`;
+
+export const CoffeeItemTitle = styled.p`
+  color: ${({ theme }) => theme["gray-800"]};
+
+  font-family: "Baloo 2";
+  font-size: 20px;
+  line-height: 26px;
+  font-weight: bold;
+  margin-top: 16px;
+`;
+
+export const CoffeeItemDescription = styled.p`
+  color: ${({ theme }) => theme["gray-600"]};
+
+  font-size: 14px;
+  line-height: 18.2px;
+  margin-top: 8px;
+`;
+
+export const CoffeeItemBuy = styled.div`
+  margin-top: 34px;
+  padding: 0 4px;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 8px;
+
+  p {
+    /* justify-self: flex-start; */
+    flex: 1;
+    color: ${({ theme }) => theme["gray-700"]};
+
+    font-family: "Baloo 2";
+    font-size: 24px;
+    line-height: 31.2px;
+    text-align: left;
+    font-weight: bold;
+  }
+
+  p span {
+    font-family: "Roboto";
+    font-size: 14px;
+    font-weight: normal;
+    line-height: 18.2px;
+  }
+
+  div {
+    background-color: ${({ theme }) => theme["gray-400"]};
+    border-radius: 6px;
+    font-size: 16px;
+    padding: 8px;
+
+    display: flex;
+    align-items: center;
+    gap: 8px;
+  }
+
+  div svg {
+    color: ${({ theme }) => theme["purple-500"]};
+  }
+
+  span > svg {
+    display: block;
+    color: ${({ theme }) => theme["white"]};
+    background-color: ${({ theme }) => theme["purple-700"]};
+    padding: 8px;
+    border-radius: 6px;
+    box-sizing: content-box;
+  }
 `;
