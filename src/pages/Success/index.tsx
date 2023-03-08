4cc67dd7a9972fae3a5dd3d68a59585a1b8b6f40
Vinicius Martins <vm3090474@gmail.com>
2023-03-07 23:02:32 -0300

    feat: create content to success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -1,3 +1,43 @@
+import { DefaultContainer } from "../Home/styles";
+import imgSuccess from "../../assets/Illustration.png";
+
 export function Success() {
-  return <div>Success</div>;
+  return (
+    <DefaultContainer>
+      <div>
+        <article>
+          <h1>Uhu! Pedido confirmado</h1>
+          <p>Agora é só aguardar que logo o café chegará até você</p>
+        </article>
+        <article>
+          <div>
+            <span>icon</span>
+            <div>
+              <p>
+                Entrega em <span>Rua João Daniel Martinelli, 102</span>
+              </p>
+              <p>Farrapos - Porto Alegre, RS</p>
+            </div>
+          </div>
+
+          <div>
+            <span>icon</span>
+            <div>
+              <p>Previsão de entrega</p>
+              <p>20 min - 30 min</p>
+            </div>
+          </div>
+
+          <div>
+            <span>icon</span>
+            <div>
+              <p>Pagamento na entrega</p>
+              <p>Cartão de Crédito</p>
+            </div>
+          </div>
+        </article>
+      </div>
+      <img src={imgSuccess} alt="" />
+    </DefaultContainer>
+  );
 }
