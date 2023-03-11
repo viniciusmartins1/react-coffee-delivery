e555cafb5586eef1b8bf38f70050d63b3c4d51ab
Vinicius Martins <vm3090474@gmail.com>
2023-03-10 23:50:51 -0300

    feat: creating checkout page part 1

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,3 +1,24 @@
+import { DefaultContainer } from "../Home/styles";
+
 export function Checkout() {
-  return <div>Checkout</div>;
+  return (
+    <DefaultContainer>
+      <div>
+        <h2>Complete seu pedido</h2>
+        <article>
+          <div>
+            <span>icon</span>
+            <div>
+              <p>Endereço de Entrega</p>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </div>
+          <form action=""></form>
+        </article>
+      </div>
+      <div>
+        <h2>Cafés selecionados</h2>
+      </div>
+    </DefaultContainer>
+  );
 }
