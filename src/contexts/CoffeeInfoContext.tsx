1d3d35dc4a8535b36e097a70fe0ff1887f217cb9
Vinicius Martins <vm3090474@gmail.com>
2023-04-11 16:45:28 -0300

    feat: add new function in context

--- src/contexts/CoffeeInfoContext.tsx
+++ src/contexts/CoffeeInfoContext.tsx
@@ -3,9 +3,11 @@ import { dataCoffee, ItemCoffee } from "../dataCoffee";
 
 interface CoffeeContextType {
   coffeeList: ItemCoffee[];
+  selectedCoffees: ItemCoffee[];
   quantityShopping: number;
   addCoffeeItem: (id: string) => void;
   removeCoffeeItem: (id: string) => void;
+  removeSelectedItem: (id: string) => void;
 }
 
 export const CoffeeContext = createContext({} as CoffeeContextType);
@@ -16,6 +18,7 @@ interface CoffeeContextProps {
 
 export function CoffeeContextProvider({ children }: CoffeeContextProps) {
   const [coffeeList, setCoffeeList] = useState<ItemCoffee[]>(dataCoffee);
+  const [selectedCoffees, setSelectedCoffees] = useState<ItemCoffee[]>([]);
   const [quantityShopping, setQuantityShopping] = useState<number>(0);
 
   function addCoffeeItem(id: string) {
@@ -38,14 +41,32 @@ export function CoffeeContextProvider({ children }: CoffeeContextProps) {
     setCoffeeList([...newArray]);
   }
 
+  function removeSelectedItem(id: string) {
+    const newArray = coffeeList.map((item) => {
+      if (item.id === id) {
+        item.quantity = 0;
+      }
+      return item;
+    });
+    setCoffeeList([...newArray]);
+  }
+
   useEffect(() => {
     const arrayToShopping = coffeeList.filter((item) => item.quantity > 0);
+    setSelectedCoffees(arrayToShopping);
     setQuantityShopping(arrayToShopping.length);
   }, [coffeeList]);
 
   return (
     <CoffeeContext.Provider
-      value={{ coffeeList, quantityShopping, addCoffeeItem, removeCoffeeItem }}
+      value={{
+        coffeeList,
+        selectedCoffees,
+        quantityShopping,
+        addCoffeeItem,
+        removeCoffeeItem,
+        removeSelectedItem,
+      }}
     >
       {children}
     </CoffeeContext.Provider>
